e379628575c1ba94708144a10489560161f5568b
Elijah Windsor <ewindsor@gmail.com>
2013-07-30 18:01:01 -0700

    comitted smart.json

---
+++ package.js
@@ -0,0 +1,13 @@
+Package.describe({
+  summary: "Meteor smart package for Knox"
+});
+
+Npm.depends({
+  'knox': "0.8.5"
+});
+
+Package.on_use(function (api) {
+  api.add_files([
+    'server.js'
+  ], 'server');
+});
